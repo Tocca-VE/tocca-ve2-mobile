203600dbb60155133ec84ff55c20597b403412df
kishan <kishan.jaiswal@topsinfosolutions.com>
2021-03-04 19:23:06 +0530

    sidebar images fixes

--- src/Sidebar/Sidebar.js
+++ src/Sidebar/Sidebar.js
@@ -1,65 +1,81 @@
-import React from 'react';
-import {Container, Row, Col, Card, Form, Button, Nav, NavItem } from "react-bootstrap";
-import { withRouter } from "react-router";
-import Image from 'react-bootstrap/Image'
-/*import Sidebar from "../moduls/sidebar.js";*/
-
+import React from "react";
+import { Container, Row, Col, Button, Nav } from "react-bootstrap";
+import Clock from "../assets/ic-clock.svg";
 
 const Sidebar = () => {
-    return (
-        /*<span>Sidebar</span>*/
+  return (
+    /*<span>Sidebar</span>*/
 
-<div className="main">
-<Container>
-  <Row className="justify-content-sm-center">
-    <Col xs={12}>
-
-    
-            <Nav className="col-md-12 d-none d-md-block sidebar"
-            activeKey="/home"
-            onSelect={selectedKey => alert(`selected ${selectedKey}`)}
+    <div className="main">
+      <Container>
+        <Row className="justify-content-sm-center">
+          <Col xs={12}>
+            <Nav
+              className="col-md-12 d-none d-md-block sidebar"
+              activeKey="/home"
+              onSelect={(selectedKey) => alert(`selected ${selectedKey}`)}
             >
-            <div className="profile-wrap">
-            <div className="profile-pic">AM</div>
-            <div className="profile-name">Alex Mercer</div>
-            </div>
+              <div className="profile-wrap">
+                <div className="profile-pic">AM</div>
+                <div className="profile-name">Alex Mercer</div>
+              </div>
 
-            <div className="seperator"></div>
+              <div className="seperator"></div>
 
-                <div className="sidebar-sticky"></div>
-                <div className="navigation-wrap">
+              <div className="sidebar-sticky"></div>
+              <div className="navigation-wrap">
+                <Nav.Item>
+                  <Nav.Link eventKey="link-1">
+                    <img
+                      src={Clock}
+                      width="24"
+                      className="d-inline-block align-center"
+                      alt=""
+                    />{" "}
+                    Profile
+                  </Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Nav.Link eventKey="link-2">
+                    <img
+                      src={Clock}
+                      width="24"
+                      className="d-inline-block align-center"
+                      alt=""
+                    />{" "}
+                    Swapped Cards
+                  </Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Nav.Link eventKey="link-3">
+                    <img
+                      src={Clock}
+                      width="24"
+                      className="d-inline-block align-center"
+                      alt=""
+                    />{" "}
+                    Terms of Service
+                  </Nav.Link>
+                </Nav.Item>
+              </div>
 
-            <Nav.Item>
-            <Nav.Link eventKey="link-1">
-             <img src="./assets/ic-clock.svg" width="24" className="d-inline-block align-center" alt=""/> Profile
-		    </Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-                <Nav.Link eventKey="link-2"><img src="./assets/ic-clock.svg" width="24" className="d-inline-block align-center" alt=""/> Swapped Cards</Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-                <Nav.Link eventKey="link-3"><img src="./assets/ic-clock.svg" width="24" className="d-inline-block align-center" alt=""/> Terms of Service</Nav.Link>
-            </Nav.Item>
-            
-            </div>
-
-            <div className="nav-bottom">
-            <Button variant="link">
-            <img src="./assets/ic-clock.svg" width="24" className="d-inline-block align-center" alt=""/>Logout
-            </Button>
-            </div>
-
+              <div className="nav-bottom">
+                <Button variant="link">
+                  <img
+                    src={Clock}
+                    width="24"
+                    className="d-inline-block align-center"
+                    alt=""
+                  />
+                  Logout
+                </Button>
+              </div>
             </Nav>
-          
-
-
-
-</Col>
-  </Row>
-</Container>
-</div>
-
-    )
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
 };
 
 export default Sidebar;
