798a2ab48c3c20ec88f8cde63b9c0275566fde5a
kishan <kishan.jaiswal@topsinfosolutions.com>
2021-03-10 14:48:28 +0530

    fixes carosuel

--- src/Lobby/Lobby.js
+++ src/Lobby/Lobby.js
@@ -1,51 +1,37 @@
 import React from "react";
-import Carousel from "react-bootstrap/Carousel";
+// import Carousel from "react-bootstrap/Carousel";
 import Timer from "./Timer";
-
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from "react-responsive-carousel";
+import "./Lobby.css";
 const Lobby = () => {
   return (
     // <span>Lobby</span>
-    
+
     <div>
       <div>
         <Timer />
       </div>
-      <Carousel>
-        <Carousel.Item interval={1000}>
-          <img
-            className="d-block w-100"
-            src="holder.js/800x400?text=First slide&bg=373940"
-            alt="First slide"
-          />
-          <Carousel.Caption>
-            <h3>First slide label</h3>
-            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item interval={500}>
-          <img
-            className="d-block w-100"
-            src="holder.js/800x400?text=Second slide&bg=282c34"
-            alt="Second slide"
-          />
-          <Carousel.Caption>
-            <h3>Second slide label</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src="holder.js/800x400?text=Third slide&bg=20232a"
-            alt="Third slide"
-          />
-          <Carousel.Caption>
-            <h3>Third slide label</h3>
-            <p>
-              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-            </p>
-          </Carousel.Caption>
-        </Carousel.Item>
+      <Carousel
+        autoFocus={true}
+        showThumbs={false}
+        showStatus={false}
+        useKeyboardArrows
+      >
+        <div>
+          <img src="assets/1.jpeg" alt="" />
+          <div key="content-0" className="my-slide primary">
+            <h1>Presentation mode</h1>
+          </div>
+        </div>
+        <div>
+          <img src="assets/2.jpeg" alt="" />
+          <p className="legend">Legend 2</p>
+        </div>
+        <div>
+          <img src="assets/3.jpeg" alt="" />
+          <p className="legend">Legend 3</p>
+        </div>
       </Carousel>
     </div>
   );
