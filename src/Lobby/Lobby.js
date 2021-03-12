7eee01bd6890374fd9f30112ffac55671a9ec6ef
darshani <darshani@topsinfosolutions.com>
2021-03-12 18:07:03 +0530

    update lobby screen

--- src/Lobby/Lobby.js
+++ src/Lobby/Lobby.js
@@ -5,12 +5,15 @@ import ToccaLogo from "../assets/tocca_logo.svg";
 import Timer from "./Timer";
 import Home from "../assets/home_image.png";
 import Clock from "../assets/icons/clock-blue.svg";
-//import Marquee from "react-marquee-slider";
-import Sponcer_01 from "../assets/sponcer_01.png";
+// import Marquee from "react-marquee-slider";
 
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import { Carousel } from "react-responsive-carousel";
 import "./Lobby.css";
+import Marquee from "react-marquee-slider";
+import Sponcer_01 from "../assets/sponcer_01.png";
+import Sponcer_02 from "../assets/sponcer_02.jpg";
+import Sponcer_03 from "../assets/sponcer_03.png";
 const Lobby = () => {
   return (
     // <span>Lobby</span>
@@ -362,10 +365,104 @@ const Lobby = () => {
 <div class="spacer05"></div>
        <div className="session-wrap">
        <h3 className="session-label">Sponsors</h3>
-      <div className="sponcer-slider">
-       
+      <div className="sponcer-list-slider">
+      <Marquee
+                  direction="rtl"
+                  scatterRandomly={false}
+                  resetAfterTries={1}
+                  key="sponsors-list"
+                  velocity={5}
+                >
+
+                <div className="sponcer-list-item">
+                   <img
+                    style={{ cursor: "pointer" }}
+                  src={Sponcer_01}
+                  alt="logo"
+                  height="28"
+                  fluid
+                />
+                <div className="sponcer-description">
+                <h4>Terminus</h4>
+                <p>General discussion of the Biomedical sector, Terminus is the important 
+                    <a href="">Readmore… </a> </p>
+                    <Button variant="primary btn-visit">Visit Booth</Button>
+                </div>
 
                 </div>
+
+	              <div className="sponcer-list-item">
+                   <img
+                    style={{ cursor: "pointer" }}
+                  src={Sponcer_01}
+                  alt="logo"
+                  height="28"
+                  fluid
+                />
+                <div className="sponcer-description">
+                <h4>Terminus</h4>
+                <p>General discussion of the Biomedical sector, Terminus is the important 
+                    <a href="">Readmore… </a> </p>
+                    <Button variant="primary btn-visit">Visit Booth</Button>
+                </div>
+
+                </div>
+
+                <div className="sponcer-list-item">
+                   <img
+                    style={{ cursor: "pointer" }}
+                  src={Sponcer_01}
+                  alt="logo"
+                  height="28"
+                  fluid
+                />
+                <div className="sponcer-description">
+                <h4>Terminus</h4>
+                <p>General discussion of the Biomedical sector, Terminus is the important 
+                    <a href="">Readmore… </a> </p>
+                    <Button variant="primary btn-visit">Visit Booth</Button>
+                </div>
+
+                </div>
+
+                <div className="sponcer-list-item">
+                   <img
+                    style={{ cursor: "pointer" }}
+                  src={Sponcer_01}
+                  alt="logo"
+                  height="28"
+                  fluid
+                />
+                <div className="sponcer-description">
+                <h4>Terminus</h4>
+                <p>General discussion of the Biomedical sector, Terminus is the important 
+                    <a href="">Readmore… </a> </p>
+                    <Button variant="primary btn-visit">Visit Booth</Button>
+                </div>
+
+                </div>
+
+                <div className="sponcer-list-item">
+                   <img
+                    style={{ cursor: "pointer" }}
+                  src={Sponcer_01}
+                  alt="logo"
+                  height="28"
+                  fluid
+                />
+                <div className="sponcer-description">
+                <h4>Terminus</h4>
+                <p>General discussion of the Biomedical sector, Terminus is the important 
+                    <a href="">Readmore… </a> </p>
+                    <Button variant="primary btn-visit">Visit Booth</Button>
+                </div>
+
+                </div>
+
+                
+                </Marquee>
+
+      </div>
     </div>
 
 <div class="footer">
--- src/Lobby/Timer.js
+++ src/Lobby/Timer.js
@@ -3,9 +3,9 @@ import React from "react";
 const Timer = () => {
   return (
 <div className="timer-component">
-    <span>19 HR</span>:
-    <span>58 MIN</span>:
-    <span>36 Sec</span>
+    <span>19 <small>HR</small></span><span>:</span>
+    <span>58 <small>MIN</small></span><span>:</span>
+    <span>36 <small>Sec</small></span>
 
 </div>   
   );
